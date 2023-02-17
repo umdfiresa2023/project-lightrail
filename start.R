47b2ca1bb5ebbad3a1b3ab083c86d89c84b6a07d
samirah1 <44945336+samirah1@users.noreply.github.com>
2023-02-17 15:10:13 -0500

    Uploaded File

---
+++ start.R
@@ -0,0 +1,3 @@
+install.packages("raster")
+library("raster")
+map<-raster("C:/Users/Samirah/Downloads/aqdh-pm2-5-concentrations-contiguous-us-1-km-2000-2016-annual-geotiff/Annual-geotiff/2000.tif")
