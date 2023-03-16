755ddf0065629151ee0db67363081477721d8661
samirah1 <samirah.huda@gmail.com>
2023-03-16 02:18:07 +0000

    Create sample code for calculating pm2.5 levels from one city center with functions from the terra package

--- mean buffer city center.R
+++ mean buffer city center.R
@@ -1,14 +1,17 @@
-install.packages("terra")
-install.packages("rgeos")
-install.packages("tidyverse")
-install.packages("tmap")
-install.packages("leaflet")
-
+#install.packages("terra")
+#install.packages("rgeos")
+#install.packages("tidyverse")
+#install.packages("tmap")
+#install.packages("leaflet")
+install.packages("remotes")
+remotes::install_github("rstudio/leaflet")
 library("terra")
-# library("rgeos")
+library("rgeos")
 library("tidyverse")
 library("tmap")
 library("leaflet")
+library("maptiles")
+library("leafem")
 
 cities_latlon<-read.csv("output/cities_latlon.csv")
 
@@ -16,33 +19,38 @@ jan2000_01<-rast("input/pm25_raster/20000101.tif")
 
 i<-1
 
-city_coord<-cities_latlon[1,]
+city_coord<-cities_latlon[1,2:3]
 
-#turn city_coord into a point spatial file and create a buffer
+pts<-vect(city_coord, 
+          crs="+proj=longlat +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
 
-city_spat<-vect(city_coord, geom=c("lon", "lat"), crs="+proj=longlat +ellps=WGS84 +datum=WGS84 +units=m +no_defs", keepgeom=FALSE)
+pts_buffer<-terra::buffer(pts, width = 10000)
 
-#coordinates(city_coord) <- ~lon+lat
-#crs(city_coord)<-"+proj=longlat +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
-city_buffer<-terra::buffer(city_spat, width = 1)
+pts_extent<-terra::buffer(pts, width = 20000)
 
-#turn raster into wgs84
+plot(pts_buffer)
+
+#work with raster
+jan2000_01<-rast("input/pm25_raster/20000101.tif")
+
 raster_project <- project(jan2000_01,  "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ")
 
-ext<-ext(raster_project)
+int<-crop(raster_project, pts_buffer,
+          snap="in",
+          mask=TRUE)
 
-#crop raster within circle
-intersection<-terra::intersect(ext, city_spat)
-
-tm_shape(city_buffer) + tm_borders()
-
-m<-plot(city_buffer)
-m
+#visualize data
+plot(int, ext=ext(pts_extent))
+lines(pts_buffer)
 
 #turn raster circle into dataframe
-jan_df<-as.data.frame(intersection)
+v <- as.data.frame(values(int))
 
+names(v)<-"pm25"
+  
+#jan_df<-as.data.frame(int)
 #find pm2.5 average within the circle dataframe
-mean_jan<-jan_df %>%
-  summarize(mean_pm25 = mean(PM25))
+v2<-v %>%
+  filter(!is.na(pm25)) %>%
+  summarize(mean_pm25 = mean(pm25))
 
